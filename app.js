f2a5838cc37337c4bdfd76cd74c23a773e9087e5
tienphandev <tienphan.dev@gmail.com>
2023-02-28 21:21:46 -0500

    Create POST endpoint

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-"Hello world"
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post("/payloads", (req, res) => {
+  axios.post("url", "Email address")
+  res.send("JSON payload hit POST endpoint successfully");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
